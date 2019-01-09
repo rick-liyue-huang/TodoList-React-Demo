32f5941a2b8f9af724999a40ecb46ee9def25129
rick-liyue-huang <rick.liyue.huang@gmail.com>
2019-01-09 12:01:35 +1100

    change UI component to stateless component

--- src/TodoListUI.js
+++ src/TodoListUI.js
@@ -3,7 +3,38 @@
 import React, { Component, Fragment } from 'react';
 import { Input, Button, List } from 'antd';
 
-class TodoListUI extends Component {
+/*
+	stateless component: one component only has render() method
+	it is optimazed, because it is function only.
+
+	normally, we select UI component as stateless component.
+*/
+
+const TodoListUI = (props) => {
+	return (
+
+			<Fragment>
+				<div style={{marginTop: '10px', marginLeft: '10px'}}>
+					<Input 
+						placeholder='pls'
+						value={props.inputValue}
+						style={{width: '300px', marginRight: '10px'}}
+						onChange={props.handleInputChange} />
+
+					<Button 
+						type='primary'
+						onClick={props.handleBtnSubmit} >Submit</Button>
+				</div>
+				<List style={{width: '300px', marginTop: '10px', marginLeft: '10px'}}
+		      bordered
+		      dataSource={props.list}
+		      renderItem={(item, index) => (<List.Item onClick={() => {props.handleItemDelete(index)}} >{item}</List.Item>)}
+		    />
+			</Fragment>
+		)
+}
+
+/*class TodoListUI extends Component {
 
 	render() {
 		return (
@@ -28,6 +59,6 @@ class TodoListUI extends Component {
 			</Fragment>
 		)
 	}
-}
+}*/
 
 export default TodoListUI;
